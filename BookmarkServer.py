b453d476d7b6a1ca1f87aec59692433d69d9383b
sainisimr <simranjeet.singh@sjsu.edu>
2017-06-07 09:07:34 -0700

    Port updated for environment

--- BookmarkServer.py
+++ BookmarkServer.py
@@ -40,6 +40,7 @@
 
 import http.server
 import requests
+import os
 from urllib.parse import unquote, parse_qs
 
 memory = {}
@@ -136,6 +137,7 @@ class Shortener(http.server.BaseHTTPRequestHandler):
             self.wfile.write("Couldn't fetch URI '{}'. Sorry!".format(longuri).encode())
 
 if __name__ == '__main__':
-    server_address = ('', 8000)
+    port = int(os.environ.get('PORT', 8000))
+    server_address = ('', port)
     httpd = http.server.HTTPServer(server_address, Shortener)
     httpd.serve_forever()
